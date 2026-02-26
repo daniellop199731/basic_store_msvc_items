c69eefb57c9fae2e4d7f5335f722a962ce1eb11c
Daniel Lopez <daniellop199731@gmail.com>
2026-02-25 19:24:41 -0500

    Implementacion de OpenFeign
    -Se ajusta la implementacion de WebCliente para usar ResponseGenericObject

--- src/main/java/com/daniel/springcloud/msvc/items/application/useCase/ItemWebClientService.java
+++ src/main/java/com/daniel/springcloud/msvc/items/application/useCase/ItemWebClientService.java
@@ -6,25 +6,28 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Random;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import com.daniel.springcloud.msvc.items.application.utils.ResponseGenericObject;
 import com.daniel.springcloud.msvc.items.domain.model.Item;
 import com.daniel.springcloud.msvc.items.domain.model.Product;
-import com.daniel.springcloud.msvc.items.domain.port.in.ItemUseCaseSimple;
+import com.daniel.springcloud.msvc.items.domain.port.in.ItemUseCase;
 
 import lombok.RequiredArgsConstructor;
 
+@Primary
 @Service
 @RequiredArgsConstructor
-public class ItemWebClientService implements ItemUseCaseSimple {
+public class ItemWebClientService implements ItemUseCase {
 
     private final WebClient.Builder client;
 
-    private List<Product> products;
-    private Product product;
+    private List<Item> items;
+    private Optional<Item> item;
 
     private Random random = new Random();    
 
@@ -35,34 +38,83 @@ public class ItemWebClientService implements ItemUseCaseSimple {
     private static final String FOUND_ITEM = "Item encontrado";
 
     @Override
-    public List<Item> findAll() {
-        return this.client.build()
+    public ResponseGenericObject<List<Item>> findAll() {
+        items = this.client.build()
         .get()
         .accept(MediaType.APPLICATION_JSON)
         .retrieve()
         .bodyToFlux(Product.class)
-        .map(productAux -> new Item(productAux, random.nextInt(10) + 1))
+        .map(product -> new Item(product, random.nextInt(10) + 1))
         .collectList()
         .block();
+
+        if (items == null){
+            return new ResponseGenericObject<>(
+                true
+                , NOT_FOUND_ITEMS
+                , null
+            );
+        }        
+
+        if (items.isEmpty()){
+            return new ResponseGenericObject<>(
+                true
+                , NOT_FOUND_ITEMS
+                , null
+            );
+        }
+
+        return new ResponseGenericObject<>(
+            true
+            , FOUND_ITEMS
+            , items
+        );
     }
 
     @Override
-    public Optional<Item> findById(long id) {
+    public ResponseGenericObject<Optional<Item>> findById(long id) {
         Map<String, Long> params = new HashMap<>();
         params.put("id", id);
         try{
-            return Optional.ofNullable(
+            item = Optional.ofNullable(
                 this.client.build()
                 .get()
                 .uri("/{id}", params)
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve()
                 .bodyToMono(Product.class)
-                .map(productAux -> new Item(productAux, random.nextInt(10) + 1))
+                .map(product -> new Item(product, random.nextInt(10) + 1))
                 .block()
             );
+
+            if (item.isEmpty()){
+                return new ResponseGenericObject<>(
+                    true
+                    , NOT_FOUND_ITEM
+                    , null
+                );                
+            }            
+
+            if (item.get().getProduct().getId() == null){
+                return new ResponseGenericObject<>(
+                    true
+                    , NOT_FOUND_ITEM
+                    , null
+                );                
+            }
+
+            return new ResponseGenericObject<>(
+                true
+                , FOUND_ITEM
+                , item
+            );            
+
         } catch (HttpMessageNotWritableException ex){
-            return Optional.empty();
+            return new ResponseGenericObject<>(
+                false
+                , DEFAULT_ERROR_MESSAGE
+                , null
+            );
         }
     }
     
